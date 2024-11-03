2f634fefb82e982ac08f26ce1f352b9ab806e105
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-03 00:48:05 -0400

    add item to array button and function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react';
 
 function NameList() {
-  const [list, setList] = useState(["Name1", "Name2", "Name3"]);
+  const [list, setList] = useState(["Item 1", "Item 2", "Item 3"]);
+  const [item, setItem] = useState(() => "New Item");
+
+  const addItem = () => {
+    setList([...list, item]);
+    setItem("");
+  };
 
   return (
     <div>
       <ul>
-        {list.map((name) => (
-          <li key={name}>{name}</li>
+        {list.map((item) => (
+          <li key={item}>{item}</li>
         ))}
       </ul>
+
+      <input 
+        type="text" 
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+      />
+      <button
+        onClick={addItem}
+      >
+        Add Item
+      </button>
     </div>
   )
 }
